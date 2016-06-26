1fe3a8b059904fa98f4496b28cfee21862c82c2d
Olena Deering <olena.ivashyna@gmail.com>
2016-06-26 15:58:23 -0600

    refactor to show a list of Farscape heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,38 @@
 import { Component } from '@angular/core';
 
+export class Person {
+    id: number;
+    name: string;
+}
+
+const PEOPLE: Person[] = [
+  { id: 11, name: 'John Crichton' },
+  { id: 12, name: 'Aeryn Sun' },
+  { id: 13, name: "Pau'u Zotoh Zhaan" },
+  { id: 14, name: 'Moyah' },
+  { id: 15, name: 'Pilot' },
+  { id: 16, name: 'Lolah' },
+  { id: 17, name: 'Chianah' },
+  { id: 18, name: 'Dominar Rygel XVI' },
+  { id: 19, name: 'Scorpious' },
+  { id: 20, name: 'Stark' },
+  { id: 21, name: 'Sikozu' }
+];
+
+
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>Details about {{person.name}}</h2>
-    <div><label>ID: </label>{{person.id}}</div>
-    <div>
-        <label>Name: </label>
-        <input [(ngModel)]="person.name" placeholder="name"/>
-    </div>
+    <ul>
+        <li *ngFor="let person of people">
+            <span>{{person.name}}</span>
+        </li>
+    </ul>
     `
 })
 
 export class AppComponent {
-    title = 'My First Angular App';
-    person = {
-        id: 1,
-        name: 'Dargo'
-    }
-}
-
-
-export class Person {
-    id: number;
-    name: string;
+    title = "Farscape Heroes";
+    public people = PEOPLE;
 }
