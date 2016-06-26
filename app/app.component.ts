f485409bb178a0b4d186d0cc0536848f887abf27
Olena Deering <olena.ivashyna@gmail.com>
2016-06-25 22:25:51 -0600

    add content to app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,28 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My Angular 2 App</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>Details about {{person.name}}</h2>
+    <div><label>ID: </label>{{person.id}}</div>
+    <div>
+        <label>Name: </label>
+        <input [(ngModel)]="person.name" placeholder="name"/>
+    </div>
+    `
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = 'My First Angular App';
+    person = {
+        id: 1,
+        name: 'Dargo'
+    }
+}
+
+
+export class Person {
+    id: number;
+    name: string;
+}
